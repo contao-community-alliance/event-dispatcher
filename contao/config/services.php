c356edff98d192b00f9fdea60d8ace3f3b4585b7
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-10-25 22:11:32 +0200

    Only create EventDispatcher instance if none has been defined already and register via extend() if so.

--- contao/config/services.php
+++ contao/config/services.php
@@ -13,40 +13,48 @@
  * @filesource
  */
 
+$closure = function ($eventDispatcher) {
+	// We get either no instance at all or the previous instance or the Pimple instance.
+	if (!$eventDispatcher || ($eventDispatcher instanceof Pimple)) {
+		$eventDispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
+	}
+
+	if (isset($GLOBALS['TL_EVENTS']) && is_array($GLOBALS['TL_EVENTS'])) {
+		foreach ($GLOBALS['TL_EVENTS'] as $eventName => $listeners) {
+			foreach ($listeners as $listener) {
+				if (is_array($listener) && count($listener) === 2 && is_int($listener[1])) {
+					list($listener, $priority) = $listener;
+				}
+				else {
+					$priority = 0;
+				}
+				$eventDispatcher->addListener($eventName, $listener, $priority);
+			}
+		}
+	}
+
+	if (isset($GLOBALS['TL_EVENT_SUBSCRIBERS']) && is_array($GLOBALS['TL_EVENT_SUBSCRIBERS'])) {
+		foreach ($GLOBALS['TL_EVENT_SUBSCRIBERS'] as $eventSubscriber) {
+			if (!is_object($eventSubscriber)) {
+				if (is_callable($eventSubscriber)) {
+					$eventSubscriber = call_user_func($eventSubscriber, $eventDispatcher);
+				}
+				else {
+					$eventSubscriber = new $eventSubscriber();
+				}
+			}
+			$eventDispatcher->addSubscriber($eventSubscriber);
+		}
+	}
+
+	return $eventDispatcher;
+};
+
 /** @var Pimple $container */
 
-$container['event-dispatcher'] = $container->share(
-	function () {
-		$eventDispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
-
-		if (isset($GLOBALS['TL_EVENTS']) && is_array($GLOBALS['TL_EVENTS'])) {
-			foreach ($GLOBALS['TL_EVENTS'] as $eventName => $listeners) {
-				foreach ($listeners as $listener) {
-					if (is_array($listener) && count($listener) === 2 && is_int($listener[1])) {
-						list($listener, $priority) = $listener;
-					}
-					else {
-						$priority = 0;
-					}
-					$eventDispatcher->addListener($eventName, $listener, $priority);
-				}
-			}
-		}
-
-		if (isset($GLOBALS['TL_EVENT_SUBSCRIBERS']) && is_array($GLOBALS['TL_EVENT_SUBSCRIBERS'])) {
-			foreach ($GLOBALS['TL_EVENT_SUBSCRIBERS'] as $eventSubscriber) {
-				if (!is_object($eventSubscriber)) {
-					if (is_callable($eventSubscriber)) {
-						$eventSubscriber = call_user_func($eventSubscriber, $eventDispatcher);
-					}
-					else {
-						$eventSubscriber = new $eventSubscriber();
-					}
-				}
-				$eventDispatcher->addSubscriber($eventSubscriber);
-			}
-		}
-
-		return $eventDispatcher;
-	}
-);
+if (isset($container['event-dispatcher']))
+{
+	$container['event-dispatcher'] = $container->share($container->extend('event-dispatcher', $closure));
+} else {
+	$container['event-dispatcher'] = $container->share($closure);
+}
