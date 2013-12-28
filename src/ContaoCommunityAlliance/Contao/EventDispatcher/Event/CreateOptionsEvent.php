b13d878a076375852a1cffd6ba90567c1896d98e
Tristan Lins <tristan.lins@bit3.de>
2013-12-28 16:28:51 +0100

    Add generic event driven options callback.

---
+++ src/ContaoCommunityAlliance/Contao/EventDispatcher/Event/CreateOptionsEvent.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Event dispatcher for Contao
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  (c) 2013 Contao Community Alliance
+ * @author         Tristan Lins <tristan.lins@bit3.de>
+ * @package        event-dispatcher
+ * @license        LGPL
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\Contao\EventDispatcher\Event;
+
+use ContaoCommunityAlliance\Contao\EventDispatcher\Helper\CreateOptionsEventHelper;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ *
+ */
+class CreateOptionsEvent extends Event
+{
+	/**
+	 * @var \DataContainer
+	 */
+	protected $dataContainer;
+
+	/**
+	 * @var \ArrayObject
+	 */
+	protected $options;
+
+	function __construct(\DataContainer $dataContainer, \ArrayObject $options = null)
+	{
+		$this->dataContainer = $dataContainer;
+
+		if ($this->options) {
+			$this->options = $options;
+		}
+		else {
+			$this->options = new \ArrayObject();
+		}
+	}
+
+	/**
+	 * @return \DataContainer
+	 */
+	public function getDataContainer()
+	{
+		return $this->dataContainer;
+	}
+
+	/**
+	 * @param \ArrayObject $options
+	 */
+	public function setOptions($options)
+	{
+		$this->options = $options;
+		return $this;
+	}
+
+	/**
+	 * @return \ArrayObject
+	 */
+	public function getOptions()
+	{
+		return $this->options;
+	}
+}
---
+++ src/ContaoCommunityAlliance/Contao/EventDispatcher/Factory/CreateOptionsEventCallbackFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Event dispatcher for Contao
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  (c) 2013 Contao Community Alliance
+ * @author         Tristan Lins <tristan.lins@bit3.de>
+ * @package        event-dispatcher
+ * @license        LGPL
+ * @filesource
+ */
+
+namespace ContaoCommunityAlliance\Contao\EventDispatcher\Factory;
+
+use ContaoCommunityAlliance\Contao\EventDispatcher\Event\CreateOptionsEvent;
+use ContaoCommunityAlliance\Contao\EventDispatcher\Helper\CreateOptionsEventCallbackHelper;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+/**
+ *
+ */
+class CreateOptionsEventCallbackFactory
+{
+	/**
+	 * @param string $eventName
+	 * @param string $class
+	 */
+	static public function createCallback($eventName, $class = null)
+	{
+		$callback = function ($dc) use ($eventName, $class) {
+			if (!$class) {
+				$class = 'ContaoCommunityAlliance\Contao\EventDispatcher\Event\CreateOptionsEvent';
+			}
+
+			/** @var CreateOptionsEvent $event */
+			$event = new $class($dc);
+
+			/** @var EventDispatcher $eventDispatcher */
+			$eventDispatcher = $GLOBALS['container']['event-dispatcher'];
+			$eventDispatcher->dispatch($eventName, $event);
+
+			return $event->getOptions()
+				->getArrayCopy();
+		};
+
+		if (version_compare(VERSION, '3.2', '<')) {
+			return CreateOptionsEventCallbackHelper::registerEventCallback($callback);
+		}
+		else {
+			return $callback;
+		}
+	}
+}
