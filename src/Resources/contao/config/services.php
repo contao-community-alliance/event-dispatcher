3b25d2c0e46290b657a57b59a5469895583aa33f
Sven Baumann <baumann.sv@gmail.com>
2017-06-28 21:40:39 +0200

    Provide the right service for pimple gateway

--- src/Resources/contao/config/services.php
+++ src/Resources/contao/config/services.php
@@ -3,14 +3,15 @@
 /**
  * This file is part of contao-community-alliance/event-dispatcher.
  *
- * (c) 2013-2016 Contao Community Alliance <https://c-c-a.org>
+ * (c) 2013-2017 Contao Community Alliance <https://c-c-a.org>
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    contao-community-alliance/event-dispatcher
  * @author     Tristan Lins <tristan@lins.io>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2013-2016 Contao Community Alliance <https://c-c-a.org>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2017 Contao Community Alliance <https://c-c-a.org>
  * @license    https://github.com/contao-community-alliance/event-dispatcher/LICENSE LGPL-3.0+
  * @link       https://github.com/contao-community-alliance/event-dispatcher
  * @filesource
@@ -25,7 +26,7 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 
 // Contao 4 code.
 if ($container->isContao4()) {
-    $container->provideSymfonyService('event_dispatcher');
+    $container->provideSymfonyService('event-dispatcher', 'event_dispatcher');
     return;
 }
 
