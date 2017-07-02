25776086b61952bb88eb6c3693f76d5bffd425bb
Sven Baumann <baumann.sv@gmail.com>
2017-07-02 02:07:57 +0200

    Phpunit test  update for add configuration pass

--- tests/DependencyInjection/Compiler/AddConfiguratorPassTest.php
+++ tests/DependencyInjection/Compiler/AddConfiguratorPassTest.php
@@ -3,13 +3,14 @@
 /**
  * This file is part of contao-community-alliance/event-dispatcher.
  *
- * (c) 2013-2016 Contao Community Alliance <https://c-c-a.org>
+ * (c) 2013-2017 Contao Community Alliance <https://c-c-a.org>
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    contao-community-alliance/event-dispatcher
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2013-2016 Contao Community Alliance <https://c-c-a.org>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2017 Contao Community Alliance <https://c-c-a.org>
  * @license    https://github.com/contao-community-alliance/event-dispatcher/LICENSE LGPL-3.0+
  * @link       https://github.com/contao-community-alliance/event-dispatcher
  * @filesource
@@ -38,6 +39,7 @@ class AddConfiguratorPassTest extends \PHPUnit_Framework_TestCase
         $dispatcherDefinition = new Definition();
         $populatorDefinition = new Definition();
 
+        $container->setParameter('kernel.debug', false);
         $container->setDefinition('event_dispatcher', $dispatcherDefinition);
         $container->setDefinition('contao_community_alliance.event_dispatcher.populator', $populatorDefinition);
 
@@ -46,6 +48,8 @@ class AddConfiguratorPassTest extends \PHPUnit_Framework_TestCase
         $compilerPass->process($container);
 
         $configurator = $dispatcherDefinition->getConfigurator();
+        $this->assertTrue($container->hasParameter('kernel.debug'));
+        $this->assertNotTrue($container->getParameter('kernel.debug'));
         $this->assertInternalType('array', $configurator);
         $this->assertInstanceOf('Symfony\Component\DependencyInjection\Reference', $configurator[0]);
         $this->assertSame('contao_community_alliance.event_dispatcher.populator', (string) $configurator[0]);
@@ -65,6 +69,7 @@ class AddConfiguratorPassTest extends \PHPUnit_Framework_TestCase
         $dispatcherDefinition->setConfigurator($realConfigurator = ['Some\Class', 'someMethod']);
         $populatorDefinition = new Definition();
 
+        $container->setParameter('kernel.debug', false);
         $container->setDefinition('event_dispatcher', $dispatcherDefinition);
         $container->setDefinition('contao_community_alliance.event_dispatcher.populator', $populatorDefinition);
 
@@ -73,6 +78,8 @@ class AddConfiguratorPassTest extends \PHPUnit_Framework_TestCase
         $compilerPass->process($container);
 
         $configurator = $dispatcherDefinition->getConfigurator();
+        $this->assertTrue($container->hasParameter('kernel.debug'));
+        $this->assertNotTrue($container->getParameter('kernel.debug'));
         $this->assertInternalType('array', $configurator);
         $this->assertInstanceOf('Symfony\Component\DependencyInjection\Reference', $configurator[0]);
         $this->assertSame('contao_community_alliance.event_dispatcher.populator', (string) $configurator[0]);
